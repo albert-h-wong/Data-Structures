6434d3737997063301d0097cc05e6659d3d058c2
albert-h-wong <albe.h.wong.com>
2019-06-13 18:55:23 -0400

    binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,13 +5,47 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    x = self
+    while True:
+      if value == self.value:
+        break
+      elif value < x.value:
+        if not x.left:
+          x.left = BinarySearchTree(value)
+          break
+        else:
+          x = x.left
+      else:
+        if not x.right:
+          x.right = BinarySearchTree(value)
+          break
+        else:
+          x = x.right
+
 
   def contains(self, target):
-    pass
+    y = self
+    while True:
+      if y.value == target:
+        return True
+      elif target < y.value and y.left:
+        y = y.left
+      elif target > y.value and y.right:
+        y = y.right
+      else:
+        return False
+  
 
   def get_max(self):
-    pass
+    max_value = self.value
+    if not self.right:
+      return max_value
+    return self.right.get_max()
+
 
   def for_each(self, cb):
-    pass
+    cb(self.value)
+    if self.left:
+      self.left.for_each(cb)
+    if self.right:
+      self.right.for_each(cb)
